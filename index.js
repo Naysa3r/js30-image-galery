55075b8cb4a74c91e315b5977b912c1a61213e41
Naysay3r <ilia.mahnach@gmail.com>
2022-02-11 22:05:18 +0300

    feat: get all page image

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const parent = document.getElementById('grid');
+
+function showData(data) {
+    const el = document.createElement('div');
+    el.classList.add('img');
+    el.style.backgroundImage = `url(${data})`;
+    parent.appendChild(el);
+}
+
 async function getData() {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data['results'][0]['urls']['regular']);
+    for (let i = 0; i < data['results'].length; i++) {
+        showData(data['results'][i]['urls']['regular']);
+    }
+    console.log(data['results'].length);
+
 }
+
 getData();
 
