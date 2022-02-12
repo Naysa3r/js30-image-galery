e25558016d79d7e9d5ab1de8a1ffb4a0fd3387d2
Naysay3r <ilia.mahnach@gmail.com>
2022-02-12 18:31:54 +0300

    feat: realize remove data and search 403

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let url = 'https://api.unsplash.com/search/photos?query=nature&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 const parent = document.getElementById('grid');
+getData();
 
 function showData(data) {
     const el = document.createElement('div');
@@ -8,20 +9,38 @@ function showData(data) {
     parent.appendChild(el);
 }
 
+function removeData() {
+    let images = document.getElementsByClassName('img');
+    console.log(images.length);
+    while (images.length != 0){
+        images[0].parentNode.removeChild(images[0]);
+    }
+}
+
 async function getData() {
     const res = await fetch(url);
     const data = await res.json();
     for (let i = 0; i < data['results'].length; i++) {
         showData(data['results'][i]['urls']['regular']);
     }
-    console.log(data['results'].length);
+    if (document.getElementsByClassName('img').length == 0) {
+        const el = document.createElement('div');
+        el.classList.add('img');
+        el.style.backgroundImage = "url(./assets/403.png)";
+        parent.appendChild(el);
+    }
+    // console.log(data['results'].length);
 
 }
 
 function search(query) {
     if(event.key === 'Enter') {
+        
+        removeData();
         url = `https://api.unsplash.com/search/photos?query=${query.value}&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
-        getData();
+        getData(); 
+
     }
 }
+
 
